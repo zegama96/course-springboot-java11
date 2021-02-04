06209dab9c0d5bf53967069dd7aadeb5926a15f5
José Gama <ze.martimgama@gmail.com>
2021-02-04 17:31:07 +0000

    Service layer, component registration

---
+++ src/main/java/com/zegama/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.zegama.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.zegama.course.entities.User;
+import com.zegama.course.repositories.UserRepository;
+
+@Component
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository; 
+	
+	public List<User> findAll(){
+		return repository.findAll();				
+	}
+	
+	public User findById(Long id) {
+		Optional <User> obj =repository.findById(id);
+		return obj.get();
+	}
+
+}
