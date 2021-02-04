28be29d85ad4aada188a5473d4dcc992dd3f2674
José Gama <ze.martimgama@gmail.com>
2021-02-04 23:02:48 +0000

    Product

---
+++ src/main/java/com/zegama/course/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.zegama.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.zegama.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product,Long>  {
+	
+	
+}
