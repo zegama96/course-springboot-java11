f5ec615cc13fe4f2f56ddc4e0bca63bcc780dad0
José Gama <ze.martimgama@gmail.com>
2021-02-04 15:48:27 +0000

    H2 database, test profile, JPA

---
+++ src/main/java/com/zegama/course/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.zegama.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.zegama.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User,Long>  {
+	
+	
+}
