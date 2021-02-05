e3101461964130128ce22eebaf4af7e992fb9c17
José Gama <ze.martimgama@gmail.com>
2021-02-05 14:44:44 +0000

    Exception handling - findById

--- src/main/java/com/zegama/course/services/UserService.java
+++ src/main/java/com/zegama/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 
 import com.zegama.course.entities.User;
 import com.zegama.course.repositories.UserRepository;
+import com.zegama.course.services.exceptions.ResourceNotFoundException;
 
 @Component
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional <User> obj =repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
