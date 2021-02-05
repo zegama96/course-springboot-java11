ad988b3a4691d0230a4fb1b5dc004444066469df
José Gama <ze.martimgama@gmail.com>
2021-02-05 12:07:55 +0000

    Payment, one-to-one association

--- src/main/java/com/zegama/course/entities/Order.java
+++ src/main/java/com/zegama/course/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -34,6 +36,8 @@ public class Order implements Serializable {
 	@JoinColumn(name = "client_id")
 	private User client;
 	
+	@OneToOne(mappedBy ="order", cascade = CascadeType.ALL)
+	private Payment payment;	
 	
 	@OneToMany(mappedBy ="id.order")
 	private Set<OrderItem> items = new HashSet<>();
@@ -86,6 +90,13 @@ public class Order implements Serializable {
 		return items;
 	}
 	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
 
 	@Override
 	public int hashCode() {
