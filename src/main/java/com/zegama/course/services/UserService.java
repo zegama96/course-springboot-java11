e496d86dd189df369d5b03092f6f7feaeed57d6b
José Gama <ze.martimgama@gmail.com>
2021-02-05 15:01:59 +0000

    Exception handling - delete

--- src/main/java/com/zegama/course/services/UserService.java
+++ src/main/java/com/zegama/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 
 import com.zegama.course.entities.User;
 import com.zegama.course.repositories.UserRepository;
+import com.zegama.course.services.exceptions.DatabaseException;
 import com.zegama.course.services.exceptions.ResourceNotFoundException;
 
 @Component
@@ -30,16 +33,22 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
 		User entity = repository.getOne(id); 
-		update(entity, obj);
+		updateData(entity, obj);
 		return repository.save(entity);
 	}
 
-	private void update(User entity, User obj) {
+	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());		
